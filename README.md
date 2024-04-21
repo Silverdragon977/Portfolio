6727e4c6380a9b883c710a662c566a759219af76
Silverdragon977 <55039285+Silverdragon977@users.noreply.github.com>
2024-04-21 14:13:55 -0500

    Updated Pathing & Database
    I got the APP_ROOT & URL_ROOT working and integrated. I have also make the database file on the server. It will check to see if the tables exist and if they do exits then it echo's out that it exists otherwise it creates the database!

--- README.md
+++ README.md
@@ -4,11 +4,11 @@
 DONE ----->First setup the file structure and get a homepage working with the words "Hello World" on it
   make sure CSS and php is working correctly
 
-DONE ----->Second add webpages with basic structure that will be rewritten in php
+Second add webpages with basic structure that will be rewritten in php
 
-DONE ----->Third  Implement the navigation bar using php to allow movement to other pages
+Third  Implement the navigation bar using php to allow movement to other pages
 
-DONE ----->Fourth  Implement the list structure in php like in the ruberic
+Fourth  Implement the list structure in php like in the ruberic
 
 Fifth  make the database and create a class to interact with the database
       get the database up and running
--- index.php
+++ index.php
@@ -5,17 +5,17 @@ $config = parse_ini_file('config.ini', true);
 $environment = $config['ENVIRONMENT'];
 $URL_BASE = $config[$environment]['URL_BASE'];
 $APP_ROOT = $config[$environment]['APP_ROOT'];
-define('APP_ROOT', dirname(__FILE__));
-define('URL_BASE', $config[$environment]["URL_BASE"]);
+// define('APP_ROOT', dirname(__FILE__));
+// define('URL_BASE', $config[$environment]["URL_BASE"]);
 ////////////////////////////////////////////////////////////
 
-include_once("./src/webPages/data.php");
+include_once($APP_ROOT . "/src/webPages/data.php");
 
-include_once("./src/viewFiles/indexHead.view.php");
-include_once("./src/viewFiles/indexHeader.view.php");
-include_once("./src/viewFiles/navigation.view.php");
+include_once($APP_ROOT . "/src/viewFiles/indexHead.view.php");
+include_once($APP_ROOT . "/src/viewFiles/indexHeader.view.php");
+include_once($APP_ROOT . "/src/viewFiles/navigation.view.php");
 
-echo "Body of Homepage";
+echo "HomePage";
 
-include_once("./src/viewFiles/footer.view.php");
+include_once($APP_ROOT . "/src/viewFiles/footer.view.php");
 
